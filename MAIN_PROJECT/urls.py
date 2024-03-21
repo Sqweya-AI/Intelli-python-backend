a5319300858ce0d5ebb940a173c4b456ae56319c
saadatsamjo <65956764+saadatsamjo@users.noreply.github.com>
2024-03-21 20:26:06 +0000

    dash board API's set up

--- MAIN_PROJECT/urls.py
+++ MAIN_PROJECT/urls.py
@@ -4,10 +4,10 @@ from django.urls import path, include
 
 
 urlpatterns = [
-    # path('admin/', admin.site.urls),
+    path('admin/', admin.site.urls),
+    path('dashboard/', include('dashboard_app.urls')),
     
     path('auth/', include('auth_app.urls')),
-    path('dashboard/', include('dashboard_app.urls')),
     path('chat/', include('bot_app.urls')),
     path('home/', include('main_app.urls')), 
 ]
