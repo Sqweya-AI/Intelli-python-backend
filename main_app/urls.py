aa289230d12770f8bf69372035f6ef23a8526813
saadatsamjo <65956764+saadatsamjo@users.noreply.github.com>
2024-04-01 23:51:47 +0000

    delete reservations, edit/update reservations added

--- main_app/urls.py
+++ main_app/urls.py
@@ -4,6 +4,7 @@ from .views import *
 
 urlpatterns = [
     path('', HomePageView.as_view(), name='home-page'),
-    path('home', HomePageView.as_view(), name='home-page'),
-    path('reservations', ReservationAPIView.as_view(), name='reservations'),
+    path('home/', HomePageView.as_view(), name='home-page'),
+    path('reservations/', ReservationAPIView.as_view(), name='reservations'),
+    path('reservations/<int:pk>/', ReservationAPIView.as_view(), name='reservation-detail'),
 ]
