296a9a28c27a3c47255851dab9874a78f56c16c0
saadatsamjo <65956764+saadatsamjo@users.noreply.github.com>
2024-04-01 23:51:47 +0000

    delete reservations, edit/update reservations added

--- main_app/views.py
+++ main_app/views.py
@@ -7,20 +7,9 @@ from auth_app.views import CsrfExemptSessionAuthentication
 from .serializers import *
 from rest_framework.decorators import authentication_classes
 
-
 class HomePageView(APIView):
     def get(self, request):
         return Response({"HOME": "home page"})
-    
-
-# @api_view(['POST'])
-# @authentication_classes([CsrfExemptSessionAuthentication]) 
-# def create_reservation(request):
-#     serializer = ReservationSerializer(data=request.data)
-#     if serializer.is_valid():
-#         serializer.save()
-#         return Response({'message': 'Reservation created successfully'}, status=status.HTTP_201_CREATED)
-#     return Response({'error': 'Failed to create reservation', 'details': serializer.errors}, status=status.HTTP_400_BAD_REQUEST)
 
 @authentication_classes([CsrfExemptSessionAuthentication]) 
 class ReservationAPIView(APIView):
@@ -37,7 +26,6 @@ class ReservationAPIView(APIView):
           return Response({'message': 'Reservation created successfully', "The reservation":reservation}, status=status.HTTP_201_CREATED)
        return Response({'error': 'Failed to create reservation', 'details': serializer.errors}, status=status.HTTP_400_BAD_REQUEST)
 
-
     def get(self, request):
         reservations = ReservationModel.objects.all().order_by('-id')
         serializer = ReservationSerializer(reservations, many=True)
@@ -52,7 +40,7 @@ class ReservationAPIView(APIView):
         serializer = ReservationSerializer(reservation, data=request.data)
         if serializer.is_valid():
             serializer.save()
-            return Response(serializer.data)
+            return Response({'Success': 'Reservation updated successfully', "The reservation is now":serializer.data})
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
     def delete(self, request, pk):
@@ -62,4 +50,4 @@ class ReservationAPIView(APIView):
             return Response({'error': 'Reservation not found'}, status=status.HTTP_404_NOT_FOUND)
 
         reservation.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
+        return Response({"Success": f'{reservation.first_name}\'s Reservation Deleted! (id = {pk})' }, status=status.HTTP_204_NO_CONTENT)
