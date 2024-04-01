927426ac0097de751ed5f681258b01250872d50b
saadatsamjo <65956764+saadatsamjo@users.noreply.github.com>
2024-04-01 22:24:12 +0000

    reservation api views added (put, get, delete, post, first name and last name in the reservation model)

--- main_app/models.py
+++ main_app/models.py
@@ -1,8 +1,10 @@
 from django.db import models
 
 class ReservationModel(models.Model):
-    customer_name = models.CharField(max_length=100)
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
     customer_email = models.EmailField()
+    amount_paid = models.DecimalField(max_digits=10, decimal_places=2)
     customer_phone = models.CharField(max_length=20)
     number_of_adult_guests = models.IntegerField()
     number_of_child_guests = models.IntegerField()
