392baba27b3a024c09898284aa74761c9e565342
saadatsamjo <65956764+saadatsamjo@users.noreply.github.com>
2024-04-01 00:14:16 +0000

    build sh

--- MAIN_PROJECT/settings.py
+++ MAIN_PROJECT/settings.py
@@ -7,7 +7,7 @@ import dj_database_url
 load_dotenv()
 
 BASE_DIR = Path(__file__).resolve().parent.parent
-SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
+SECRET_KEY = os.getenv('SECRET_KEY')
 DEBUG = os.getenv('DEBUG')
 
 #MYSQL
