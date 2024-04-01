be1419b463ccf0b738e304bff690c3291d4dbbbc
saadatsamjo <65956764+saadatsamjo@users.noreply.github.com>
2024-04-01 20:49:18 +0000

    reservation api views added (put, get, delete, post)

--- main_app/views.py
+++ main_app/views.py
@@ -22,3 +22,38 @@ def create_reservation(request):
         return Response({'message': 'Reservation created successfully'}, status=status.HTTP_201_CREATED)
     return Response({'error': 'Failed to create reservation', 'details': serializer.errors}, status=status.HTTP_400_BAD_REQUEST)
 
+@authentication_classes([CsrfExemptSessionAuthentication]) 
+class ReservationAPIView(APIView):
+    def post(self, request):
+       serializer = ReservationSerializer(data=request.data)
+       if serializer.is_valid():
+          serializer.save()
+          return Response({'message': 'Reservation created successfully'}, status=status.HTTP_201_CREATED)
+       return Response({'error': 'Failed to create reservation', 'details': serializer.errors}, status=status.HTTP_400_BAD_REQUEST)
+
+
+    def get(self, request):
+        reservations = ReservationModel.objects.all()
+        serializer = ReservationSerializer(reservations, many=True)
+        return Response(serializer.data)
+
+    def put(self, request, pk):
+        try:
+            reservation = ReservationModel.objects.get(pk=pk)
+        except ReservationModel.DoesNotExist:
+            return Response({'error': 'Reservation not found'}, status=status.HTTP_404_NOT_FOUND)
+
+        serializer = ReservationSerializer(reservation, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk):
+        try:
+            reservation = ReservationModel.objects.get(pk=pk)
+        except ReservationModel.DoesNotExist:
+            return Response({'error': 'Reservation not found'}, status=status.HTTP_404_NOT_FOUND)
+
+        reservation.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
