ebaae044b0245c87197154056586b2b4f52535ad
saadatsamjo <65956764+saadatsamjo@users.noreply.github.com>
2024-05-24 13:59:27 +0000

    email properly set

--- auth_app/utils.py
+++ auth_app/utils.py
@@ -6,17 +6,18 @@ load_dotenv()
 
 resend.api_key= os.getenv('resend_api_key')
  
-variable1 = 'User One'
-variable2 = 'User Two'
+first_name = 'First Name'
 
 def send_reset_password_email(email, reset_token):
     r = resend.Emails.send({
     "from": "support@medivarse.com",
     "to": email,
     "subject": "Reset your password!",
-    "html":f'<p><strong>Pasword Reset </strong> link for <strong> {variable1} </strong></p>' 
-        f'<p>{reset_token}</p>' 
-        '<strong>Rapid Rabbit </strong>'
+    "html":f'<p> Hello [{first_name}  We are sorry to hear that you have been having trouble logging in on  our Intelli Concierge.\
+          To resett your password, click the link below</p>' 
+        f'<p>https://intelli-python-backend.onrender.com/auth/reset_password/{reset_token}</p>' 
+        'You can only use this link once, not to be shared to anyone'
+        '<strong> Intelli Concierge </strong>'
     })
 
 def send_verification_email(email, verification_code):
@@ -24,8 +25,9 @@ def send_verification_email(email, verification_code):
     "from": "support@medivarse.com",
     "to": email,
     "subject": "Verify your email!",
-    "html":f'<p><strong>Email verification</strong> code \
-        <strong> {variable2} </strong></p>' 
-        f'<p>{verification_code}</p>' 
-        '<strong>Rapid Rabbit </strong>'
+    "html":f'<p>Hello {first_name}.\
+        Welcome to Intelli Concierge.\
+        To verify your account, use the code below when prompted to enter your verification code.\
+        This code is meant to not be shared to anyone
+        <strong> Intelli Concierge </strong>'  
     })
