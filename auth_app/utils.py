ba3eed4b41fbb5224cb8d7f0508cdafea4d20f21
saadatsamjo <65956764+saadatsamjo@users.noreply.github.com>
2024-05-24 13:38:31 +0000

    build error fixed

--- auth_app/utils.py
+++ auth_app/utils.py
@@ -5,14 +5,16 @@ from dotenv import load_dotenv
 load_dotenv()
 
 resend.api_key= os.getenv('resend_api_key')
-
+ 
+variable1 = 'User One'
+variable2 = 'User Two'
 
 def send_reset_password_email(email, reset_token):
     r = resend.Emails.send({
     "from": "support@medivarse.com",
     "to": email,
     "subject": "Reset your password!",
-    "html":f'<p><strong>Pasword Reset </strong> link for <strong> {'variable'} </strong></p>' 
+    "html":f'<p><strong>Pasword Reset </strong> link for <strong> {variable1} </strong></p>' 
         f'<p>{reset_token}</p>' 
         '<strong>Rapid Rabbit </strong>'
     })
@@ -23,7 +25,7 @@ def send_verification_email(email, verification_code):
     "to": email,
     "subject": "Verify your email!",
     "html":f'<p><strong>Email verification</strong> code \
-        <strong> {'variable'} </strong></p>' 
+        <strong> {variable2} </strong></p>' 
         f'<p>{verification_code}</p>' 
         '<strong>Rapid Rabbit </strong>'
     })
