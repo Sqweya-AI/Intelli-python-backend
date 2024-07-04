e8c4860ce7e495dd5b24f29b3263bc1a827814f4
imKaba <mahamadou.kaba@meltwater.org>
2024-07-04 00:11:20 +0000

    csrf exempted

--- appservice/views.py
+++ appservice/views.py
@@ -1,5 +1,10 @@
 from django.http import HttpResponse, JsonResponse
 from rest_framework.response import Response
+from rest_framework.decorators import api_view, authentication_classes, permission_classes
+from rest_framework.permissions import AllowAny
+
+from django.views.decorators.csrf import csrf_exempt
+
 
 import os 
 
@@ -18,12 +23,19 @@ OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
 VERIFY_TOKEN = os.getenv("VERIFY_TOKEN")
 
 
+
+@api_view(['POST'])
+@permission_classes([AllowAny,])
+@csrf_exempt
 def webhook(request):
     if request.method == 'GET':
         print(request.GET)
+
         mode = request.GET.get('hub.mode')
         token = request.GET.get('hub.verify_token')
         challenge = request.GET.get('hub.challenge')
+
+        print(mode, token, challenge)
         
         logging.info(f"Verification attempt - Mode: {mode}, Token: {token}, Challenge: {challenge}")
         logging.info(f"Stored VERIFY_TOKEN: {VERIFY_TOKEN}")
@@ -32,8 +44,7 @@ def webhook(request):
         if mode == 'subscribe' and token == VERIFY_TOKEN:
             logging.info("Verification successful")
             return HttpResponse(challenge, content_type="text/plain")
-            response["Access-Control-Allow-Origin"] = "*"
-            return response
+ 
         else:
             logging.warning("Verification failed")
             return JsonResponse({'error': 'Verification token mismatch'}, status=403)
