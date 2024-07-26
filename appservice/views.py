13789963c33cbfa1057ca44091f6cdaa8f1abec0
imKaba <mahamadou.kaba@meltwater.org>
2024-07-26 16:17:11 +0000

    auditing the double messaging issue 4

--- appservice/views.py
+++ appservice/views.py
@@ -48,104 +48,141 @@ def get_chat_history(chatsession):
     return chat_history
 
 
-# def handle_verification(request) -> HttpResponse:
-#     mode      = request.GET.get('hub.mode')
-#     token     = request.GET.get('hub.verify_token')
-#     challenge = request.GET.get('hub.challenge')
+def handle_verification(request) -> HttpResponse:
+    mode      = request.GET.get('hub.mode')
+    token     = request.GET.get('hub.verify_token')
+    challenge = request.GET.get('hub.challenge')
 
-#     logger.info(f"Verification attempt - Mode: {mode}, Token: {token}, Challenge: {challenge}")
-#     logger.info(f"Stored VERIFY_TOKEN: {VERIFY_TOKEN}")
+    logger.info(f"Verification attempt - Mode: {mode}, Token: {token}, Challenge: {challenge}")
+    logger.info(f"Stored VERIFY_TOKEN: {VERIFY_TOKEN}")
 
-#     if mode == 'subscribe' and token == VERIFY_TOKEN:
-#         logger.info("Verification successful")
-#         return HttpResponse(challenge, content_type="text/plain")
-#     else:
-#         logger.warning("Verification failed")
-#         return JsonResponse({'error': 'Verification token mismatch'}, status=status.HTTP_403_FORBIDDEN)
-
-
-# def extract_whatsapp_data(data: Dict[str, Any]) -> Dict[str, Any]:
-#     try:
-#         entry   = data['entry'][0]
-#         changes = entry['changes'][0]['value']
-#         return {
-#             'id': entry['id'],
-#             'customer_number': changes['contacts'][0]['wa_id'],
-#             'customer_name'  : changes['contacts'][0]['profile']['name'],
-#             'content'        : changes['messages'][0]['text']['body']
-#         }
-#     except KeyError:
-#         status = changes['statuses'][0]['status']
-#         logger.info(f"Received status: {status}")
-#         return {'status': status}
+    if mode == 'subscribe' and token == VERIFY_TOKEN:
+        logger.info("Verification successful")
+        return HttpResponse(challenge, content_type="text/plain")
+    else:
+        logger.warning("Verification failed")
+        return JsonResponse({'error': 'Verification token mismatch'}, status=status.HTTP_403_FORBIDDEN)
 
 
-# def handle_whatsapp_message(data: Dict[str, Any]) -> JsonResponse:
-#     appservice = get_object_or_404(AppService, whatsapp_business_account_id=data['id'])
-#     chatsession, created = ChatSession.objects.get_or_create(
-#         customer_number=data['customer_number'],
-#         appservice=appservice,
-#     )
-
-#     if created:
-#         chatsession.customer_name = data['customer_name']
-#         chatsession.save()
-
-#     if not chatsession.is_handle_by_human and data['content']:
-#         answer = bot_process(
-#             input_text=data['content'],
-#             appservice=appservice,
-#             recipient_id=data['customer_number'],
-#             assistant_id=appservice.assistant_id
-#         )
-#     else:
-#         answer = 'Please wait for our response..'
-
-#     send_whatsapp_message({
-#         "recipient": data['customer_number'],
-#         "text": answer,
-#         "phone_number_id": appservice.phone_number_id,
-#         "access_token": appservice.access_token
-#     })
-
-#     Message.objects.create(
-#         content=data['content'],
-#         answer=answer,
-#         chatsession=chatsession,
-#         sender='ai'
-#     )
-
-#     return JsonResponse({'result': answer}, status=status.HTTP_201_CREATED)
+def extract_whatsapp_data(data: Dict[str, Any]) -> Dict[str, Any]:
+    try:
+        entry   = data['entry'][0]
+        changes = entry['changes'][0]['value']
+        if 'messages' in changes:
+            return {
+                'id': entry['id'],
+                'customer_number': changes['contacts'][0]['wa_id'],
+                'customer_name'  : changes['contacts'][0]['profile']['name'],
+                'content'        : changes['messages'][0]['text']['body']
+            }
+    except KeyError:
+        status = changes['statuses'][0]['status']
+        logger.info(f"Received status: {status}")
+        return {'status': status}
 
 
-# def handle_other_message(data: Dict[str, Any]) -> JsonResponse:
-#     appservice = get_object_or_404(AppService, phone_number=data['phone_number'])
-#     chatsession, _ = ChatSession.objects.get_or_create(
-#         customer_number=data['customer_number'],
-#         appservice=appservice,
-#     )
+def handle_whatsapp_message(data: Dict[str, Any]) -> JsonResponse:
+    appservice = get_object_or_404(AppService, whatsapp_business_account_id=data['id'])
+    chatsession, created = ChatSession.objects.get_or_create(
+        customer_number=data['customer_number'],
+        appservice=appservice,
+    )
 
-#     send_whatsapp_message({
-#         "recipient": data['customer_number'],
-#         "text": data['answer'],
-#         "phone_number_id": appservice.phone_number_id,
-#         "access_token": appservice.access_token
-#     })
+    if created:
+        chatsession.customer_name = data['customer_name']
+        chatsession.save()
 
-#     Message.objects.create(
-#         content=data['content'],
-#         answer=data['answer'],
-#         chatsession=chatsession,
-#         sender='human'
-#     )
+    if not chatsession.is_handle_by_human and data['content']:
+        answer = bot_process(
+            input_text=data['content'],
+            appservice=appservice,
+            recipient_id=data['customer_number'],
+            assistant_id=appservice.assistant_id
+        )
+    else:
+        answer = 'Please wait for our response..'
 
-#     return JsonResponse({'result': data['answer']}, status=status.HTTP_201_CREATED)
+    send_whatsapp_message({
+        "recipient": data['customer_number'],
+        "text": answer,
+        "phone_number_id": appservice.phone_number_id,
+        "access_token": appservice.access_token
+    })
 
-# @api_view(['GET', 'POST'])
-# @permission_classes([AllowAny])
+    Message.objects.create(
+        content=data['content'],
+        answer=answer,
+        chatsession=chatsession,
+        sender='ai'
+    )
+
+    return JsonResponse({'result': answer}, status=status.HTTP_201_CREATED)
+
+
+def handle_other_message(data: Dict[str, Any]) -> JsonResponse:
+    appservice = get_object_or_404(AppService, phone_number=data['phone_number'])
+    chatsession, _ = ChatSession.objects.get_or_create(
+        customer_number=data['customer_number'],
+        appservice=appservice,
+    )
+
+    send_whatsapp_message({
+        "recipient": data['customer_number'],
+        "text": data['answer'],
+        "phone_number_id": appservice.phone_number_id,
+        "access_token": appservice.access_token
+    })
+
+    Message.objects.create(
+        content=data['content'],
+        answer=data['answer'],
+        chatsession=chatsession,
+        sender='human'
+    )
+
+    return JsonResponse({'result': data['answer']}, status=status.HTTP_201_CREATED)
+
+@api_view(['GET', 'POST'])
+@permission_classes([AllowAny])
+def webhook(request):
+    if request.method == 'GET':
+        return handle_verification(request)
+    
+    elif request.method == 'POST':
+        data = request.data
+        
+        if 'object' in data and 'entry' in data:
+            whatsapp_data = extract_whatsapp_data(data)
+            if 'status' in whatsapp_data:
+                return Response({'result': "Status Well Received"}, status=status.HTTP_200_OK)
+            return handle_whatsapp_message(whatsapp_data)
+        else:
+            return handle_other_message(data)
+        
+
+
+# count = 0
+# @api_view(['GET','POST'])
+# @permission_classes([AllowAny,])
+# @csrf_exempt
 # def webhook(request):
 #     if request.method == 'GET':
-#         return handle_verification(request)
+        
+#         mode      = request.GET.get('hub.mode')
+#         token     = request.GET.get('hub.verify_token')
+#         challenge = request.GET.get('hub.challenge')
+
+        
+#         logging.info(f"Verification attempt - Mode: {mode}, Token: {token}, Challenge: {challenge}")
+#         logging.info(f"Stored VERIFY_TOKEN: {VERIFY_TOKEN}")
+
+#         if mode == 'subscribe' and token == VERIFY_TOKEN:
+#             logging.info("Verification successful")
+#             return HttpResponse(challenge, content_type="text/plain")
+ 
+#         else:
+#             logging.warning("Verification failed")
+#             return JsonResponse({'error': 'Verification token mismatch'}, status=403)
     
 #     elif request.method == 'POST':
 #         data = request.data
