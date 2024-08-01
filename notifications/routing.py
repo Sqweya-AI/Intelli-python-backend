6fc9781288e3d3536aac63aee362829e1ceefa6b
imKaba <mahamadou.kaba@meltwater.org>
2024-08-01 14:50:37 +0000

    fix: notification base on user_channel identified by phone_number of appservice

--- notifications/routing.py
+++ notifications/routing.py
@@ -1,6 +1,17 @@
 from django.urls import re_path
 from . import consumers
 
+# websocket_urlpatterns = [
+#     re_path(r'^ws/events/<str:phone_number>/', consumers.EventConsumer.as_asgi()),
+# ]
+
 websocket_urlpatterns = [
-    re_path(r'^ws/events/<str:phone_number>/', consumers.EventConsumer.as_asgi()),
+    re_path(r'^ws/events/(?P<phone_number>\w+)/$', consumers.EventConsumer.as_asgi()),
 ]
+
+
+
+
+"""
+uvicorn MAIN_PROJECT.asgi:application --host 0.0.0.0 --port 8000
+"""
